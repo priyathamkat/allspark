bba47cf97cb93c036369cec7430f5e40f9b2dfcf
priyathamkat <priyathamkat@gmail.com>
2024-03-08 17:07:30 -0500

    Implement positional encoding as an `nn.Module`

---
+++ allspark/layers/positional_encoding.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, max_seq_len: int) -> None:
+        super().__init__()
+
+        positions = torch.arange(max_seq_len).unsqueeze(-1)
+
+        dimensions = 2 * torch.arange((d_model + 1) // 2)
+        dimensions = dimensions.unsqueeze(0)
+
+        frequencies = 1 / (10000 ** (dimensions / d_model))
+        arguments = positions * frequencies
+
+        positional_encoding = torch.zeros(max_seq_len, d_model)
+        positional_encoding[:, ::2] = torch.sin(arguments)
+        positional_encoding[:, 1::2] = torch.cos(arguments[:, : d_model // 2])
+
+        self.register_buffer("positional_encoding", positional_encoding)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x + self.positional_encoding[: x.shape[-2], :].to(x)
