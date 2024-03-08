a33c4f5ab1b09594a0217db3e8a967552af7a252
priyathamkat <priyathamkat@gmail.com>
2024-03-08 08:40:36 -0500

    Add `MultiHeadAttention`

--- allspark/layers/attention.py
+++ allspark/layers/attention.py
@@ -6,6 +6,8 @@ import torch.nn.functional as F
 class ScaledDotProductAttention(nn.Module):
     """Computes scaled dot-product attention as defined in https://arxiv.org/pdf/1706.03762.pdf.
 
+    This layer has no learnable parameters.
+
     Args:
         is_causal (bool, optional): If True, applies a causal mask to the attention weights.
         dropout (float, optional): Dropout probability applied to the attention weights.
@@ -36,8 +38,62 @@ class ScaledDotProductAttention(nn.Module):
         d = key.shape[-1]
         weights = torch.matmul(query, key.transpose(-2, -1)) / (d**0.5)
         if self.is_causal:
-            mask = torch.ones(1, *weights.shape[-2:]).tril_()
+            mask = torch.ones(*weights.shape[-2:]).tril_()
             weights.masked_fill_(mask == 0, float("-inf"))
         weights = F.softmax(weights, dim=-1)
         weights = self.dropout(weights)
         return torch.matmul(weights, value)
+
+
+def init_xavier_normal_parameter(shape: torch.Size) -> nn.Parameter:
+    data = torch.empty(shape)
+    nn.init.xavier_normal_(data)
+    return nn.Parameter(data)
+
+
+class MultiHeadAttention(nn.Module):
+    """Computes multi-head attention as defined in https://arxiv.org/pdf/1706.03762.pdf.
+
+    Args:
+        num_heads (int): Number of attention heads.
+        d_model (int): Dimensionality of model embeddings.
+        d_k (int): Dimensionality of the key tensor.
+        d_v (int): Dimensionality of the value tensor.
+        is_causal (bool, optional): If True, applies a causal mask to the attention weights.
+        dropout (float, optional): Dropout probability applied to the attention weights.
+
+    Multi-head attention is defined as:
+        MultiHead(Q, K, V) = Concat(head_1, ..., head_h)W_o
+            where head_i = Attention(QW^Q_i, KW^K_i, VW^V_i)
+                  W^Q_i, W^K_i, W^V_i, W_o are learnable parameters
+                  W_o is a learnable parameter that projects the concatenated attention heads to the output space
+
+    Returns:
+        torch.Tensor: The multi-head attention tensor (shape: [batch_size, num_queries, d_model])
+
+    """
+
+    def __init__(
+        self,
+        num_heads: int,
+        d_model: int,
+        d_k: int,
+        d_v: int,
+        is_causal: bool = False,
+        dropout: float = 0.0,
+    ) -> None:
+        super().__init__()
+        self.w_q = init_xavier_normal_parameter((num_heads, d_model, d_k))
+        self.w_k = init_xavier_normal_parameter((num_heads, d_model, d_k))
+        self.w_v = init_xavier_normal_parameter((num_heads, d_model, d_v))
+        self.w_o = init_xavier_normal_parameter((d_model, num_heads * d_v))
+        self.attention = ScaledDotProductAttention(is_causal, dropout)
+
+    def forward(self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor):
+        query = torch.matmul(query.unsqueeze(1), self.w_q)
+        key = torch.matmul(key.unsqueeze(1), self.w_k)
+        value = torch.matmul(value.unsqueeze(1), self.w_v)
+        output = self.attention(query, key, value)
+        output = output.reshape((output.shape[0], -1, output.shape[-1]))
+        output = torch.matmul(self.w_o, output)
+        return output
